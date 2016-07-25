0e94e6de6b96bd91d29d3ecfdaab1e70a6c8ea14
Raphaël Choquet <rchoquet@lacoste.com>
2016-07-25 17:17:28 +0200

    add body parser

--- server/drivers/server.js
+++ server/drivers/server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const http = require('http');
+const bodyParser = require('body-parser');
 
 module.exports = function(app) {
 
     return {
         instance: null,
         express: null,
+        port: null,
 
         /// FUNCTION ///
         create: function() {
             this.express = express();
+            this.port = process.env.PORT || 8080;
+            this.express.use(bodyParser.urlencoded({ extended: true }));
+            this.express.use(bodyParser.json());
             this.instance = http.Server(this.express);
             app.views.setup();
             app.routes.create();
@@ -17,8 +22,8 @@ module.exports = function(app) {
         },
 
         listen: function() {
-            this.instance.listen(app.config.port, function() {
-                console.log('Server listening on *:' + app.config.port);
+            this.instance.listen(this.port, function() {
+                console.log('Server listening on *:' + this.port);
             });
         }
     };
